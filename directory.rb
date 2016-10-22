88d4c224597548c766c67fc6e5963d3344d2e116
Dan Pelensky <dan@pelensky.com>
2016-10-22 12:25:10 +0100

    cohort added

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 
 # array of students
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone",:november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator",:november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
 
 #method to print the header
@@ -21,13 +21,15 @@ def print_header
 end
 
 #method to print the students names, one on each line
-def print(names)
-  puts names.each {|student| puts student}
+def print(students)
+  students.each  do |student|
+    puts "#{student[0]} (#{student[1].capitalize} cohort)"
+  end
 end
 
 #method to print the footer
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 #call the methods

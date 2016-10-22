ff36bbbaad64d5fb99b8682dfa77bbe350c1bcbe
Dan Pelensky <dan@pelensky.com>
2016-10-22 11:40:32 +0100

    total number of students added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#List of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Kruger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Total number of students
+print "Overall, we have "
+print 11
+puts " great students"

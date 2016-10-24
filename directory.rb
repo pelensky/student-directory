f3de5757718a3a2be7b32ed9119caff089437a9c
Dan Pelensky <dan@pelensky.com>
2016-10-24 10:44:36 +0100

    output centered (50 px) - ex 8.6

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 def input_students
-  puts "Please enter the name of the first student \n>>"
+  puts "Please enter the name of the first student \n>>".center(50)
   #create an empty array
   students = []
   #get the first name
@@ -9,7 +9,7 @@ def input_students
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(50)
     #get another name from the user
     name = gets.chomp
   end
@@ -18,20 +18,20 @@ end
 
 #method to print the header
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(20)
+  puts "-------------".center(20)
 end
 
-
+#center output
 def print(students)
-  students.each_with_index  do |student, index|
-    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort."
+   students.each_with_index  do |student, index|
+    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort.".center(50)
   end
 end
 
 #method to print the footer
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 #call the methods

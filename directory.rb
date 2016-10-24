848d9ad0734fb288ba68192b9de2edaca5ff76af
Dan Pelensky <dan@pelensky.com>
2016-10-24 10:29:43 +0100

    added additional info like hobbies weight etc (8.5)

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of the first student \n>>"
   #create an empty array
   students = []
   #get the first name
   name = gets.chomp
+  puts "What hobbies do they have? \n>>"
+  hobbies = gets.chomp
+  puts "What is their country of birth? \n>>"
+  country = gets.chomp
+  puts "How tall are they in cm? \n>> "
+  height = gets.chomp
+  puts "How much do they weigh in kg? \n>>"
+  weight = gets.chomp
+
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: hobbies, country: country, height: height, weight: weight}
     puts "Now we have #{students.count} students"
     #get another name from the user
+    puts "Please enter the next of the students \n>>"
     name = gets.chomp
+    puts "What hobbies do they have? \n>>"
+    hobbies = gets.chomp
+    puts "What is their country of birth? \n>>"
+    country = gets.chomp
+    puts "How tall are they in cm? \n>> "
+    height = gets.chomp
+    puts "How much do they weigh in kg? \n>>"
+    weight = gets.chomp
   end
   students
 end
@@ -23,10 +40,9 @@ def print_header
 end
 
 
-#change each method to a loop (Exercise 8.4)
 def print(students)
-  for student in students do
-    puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
+  students.each_with_index  do |student, index|
+    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort, from #{student[:country]}, who's hobbies are #{student[:hobbies]}. Finally, they are #{student[:height]} cm tall and weight #{student[:weight]} kgs."
   end
 end
 

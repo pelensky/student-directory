08deb130f9fec5e7f4921669186aafbe5c6119f8
Dan Pelensky <dan@pelensky.com>
2016-10-24 10:34:21 +0100

    removed the extra info from exercise 8.5 to have a blank slate to move on to 8.6

--- directory.rb
+++ directory.rb
@@ -4,31 +4,14 @@ def input_students
   students = []
   #get the first name
   name = gets.chomp
-  puts "What hobbies do they have? \n>>"
-  hobbies = gets.chomp
-  puts "What is their country of birth? \n>>"
-  country = gets.chomp
-  puts "How tall are they in cm? \n>> "
-  height = gets.chomp
-  puts "How much do they weigh in kg? \n>>"
-  weight = gets.chomp
 
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: hobbies, country: country, height: height, weight: weight}
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     #get another name from the user
-    puts "Please enter the next of the students \n>>"
     name = gets.chomp
-    puts "What hobbies do they have? \n>>"
-    hobbies = gets.chomp
-    puts "What is their country of birth? \n>>"
-    country = gets.chomp
-    puts "How tall are they in cm? \n>> "
-    height = gets.chomp
-    puts "How much do they weigh in kg? \n>>"
-    weight = gets.chomp
   end
   students
 end
@@ -42,7 +25,7 @@ end
 
 def print(students)
   students.each_with_index  do |student, index|
-    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort, from #{student[:country]}, who's hobbies are #{student[:hobbies]}. Finally, they are #{student[:height]} cm tall and weight #{student[:weight]} kgs."
+    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort."
   end
 end
 

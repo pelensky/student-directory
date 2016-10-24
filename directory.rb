38884a3e97cbed7ba14a90bd35d55738566c9260
Dan Pelensky <dan@pelensky.com>
2016-10-24 11:48:06 +0100

    make cohort more in depth, assign value of November if not stated, check if month is spelled correctly

--- directory.rb
+++ directory.rb
@@ -1,37 +1,62 @@
 def input_students
-  puts "Please enter the name of the first student \n>>".center(50)
+  puts "Please enter the name of the first student."
+  puts "Type 'quit' when you are finished."
   #create an empty array
   students = []
   #get the first name
   name = gets.chomp
+  #get the cohort
+  puts "What cohort are they in?"
+  cohort = gets.chomp
+    if cohort.empty?
+      cohort = "November"
+    else
+      cohort = cohort
+    end
 
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while true do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students".center(50)
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students"
     #get another name from the user
+    puts "Please enter the name of the next student."
     name = gets.chomp
+    #break if the user writes quit
+    break if name.downcase == "quit"
+    puts "What cohort are they in?"
+    cohort = gets.chomp
+    #assign the value of November if blank
+      if cohort.empty?
+        cohort = "November"
+        #validate the month
+      elsif cohort == "January" || cohort == "February" || cohort == "March" || cohort == "April" || cohort == "May" || cohort == "June" || cohort == "July" || cohort == "August" || cohort == "September" || cohort == "October" || cohort == "November" || cohort == "December"
+        cohort = cohort
+      else
+        #ask them to re-enter the value if incorrect
+        puts "Was that spelled correctly? Please enter the cohort again."
+        cohort = gets.chomp
+      end
   end
   students
 end
 
 #method to print the header
 def print_header
-  puts "The students of Villains Academy".center(20)
+  puts "The students of Villains Academy"
   puts "-------------".center(20)
 end
 
 #center output
 def print(students)
    students.each_with_index  do |student, index|
-    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort.".center(50)
+    puts "#{index + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort)."
   end
 end
 
 #method to print the footer
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50)
+  puts "Overall, we have #{students.count} great students"
 end
 
 #call the methods

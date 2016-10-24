bbf78ed8bd6fc436a33434435de51182a0daf27a
Dan Pelensky <dan@pelensky.com>
2016-10-24 13:49:21 +0100

    interactive menu added - step 9

--- directory.rb
+++ directory.rb
@@ -1,24 +1,44 @@
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
-  # create an empty array
+  def interactive_menu
   students = []
 
-  # get the first name
-  name = gets.chomp
+  loop do
+    #1) print the menu and ask the use what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    #2) read the input and save it into a variable
+    selection = gets.chomp
+    #3) do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      if students.length >= 1
+      print_header
+      print_students(students)
+      print_footer(students)
+      else
+        puts "You have not yet added any students. Please add a student to see the list."
+      end
+    when "9"
+      exit #this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again."
+    end
+  end
+end
 
-  # while the name is not empty, repeat this code
+def input_students
+  students = []
+  puts "Please enter a name."
+  name = gets.chomp
   while !name.empty? do
-  # add the student hash to the array
   students << {name: name, cohort: :november}
   puts "Now we have #{students.count} students"
-
-  #get another name from the user
   name = gets.chomp
   end
-
-  #return the array of students
   students
+end
 
 def print_header
   puts "The students of my cohort at Makers Academy"
@@ -35,10 +55,4 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-if students.length >= 1
-print_header
-print_students(students)
-print_footer(students)
-else
-  puts "You have not yet added any students. Please add a student to see the list."
-end
+interactive_menu

a85600de3a2db7d74a54b166aaa05ac518fb9436
Dan Pelensky <dan@pelensky.com>
2016-10-25 11:34:19 +0100

    try_load_students method defined and working. Ex 13

--- directory.rb
+++ directory.rb
@@ -1,9 +1,10 @@
   @students = []
 
+
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -35,11 +36,11 @@ end
 def input_students
   puts "Please enter the name of a student."
   puts "To finish, just hit return twice."
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
   @students << {name: name, cohort: :november}
   puts "Now we have #{@students.count} students"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   end
 end
 
@@ -84,8 +85,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -93,4 +94,16 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exists
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+try_load_students
 interactive_menu
